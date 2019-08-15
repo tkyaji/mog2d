ec02a8584ba27ca4f9bbbc66af518fc7167ee98b
tkyaji <tkyaji@gmail.com>
2019-08-15 22:06:59 +0900

    delete debug code.

--- sources/app/SampleScene.cpp
+++ sources/app/SampleScene.cpp
@@ -4,21 +4,4 @@ using namespace mog;
 
 void SampleScene::onLoad() {
     
-    auto group = Group::create();
-    group->setSize(Screen::getSize() * 0.5f);
-    group->setAnchor(1.0f, 1.0f);
-    group->setPivot(1.0f, 1.0f);
-
-    
-    auto sprite = Sprite::create("test.png");
-    group->add(sprite);
-    
-    auto label = Label::create("test", 30);
-    group->add(label);
-
-    sprite->setAnchor(1.0f, 1.0f);
-    sprite->setPivot(1.0f, 1.0f);
-
-    this->add(group);
-    
 }
